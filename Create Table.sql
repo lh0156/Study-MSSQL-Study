fa5640703ba7fd021335c4eda9657cfa1d3f40b5
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-24 10:41:45 +0900

    littlt add Create Table commit

--- Create Table.sql
+++ Create Table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE dbo.memberTBL(
-	member_id varchar(16) primary key,
+	mem_id varchar(16) primary key,
 	mem_name nchar(10) not null,
 	mem_gender char(1) not null,
 	mem_join date not null,
-	mem_eamil varchar(98)
+	mem_email varchar(98)
 );
+
+-- 디폴트는 내 PC -> 문서 -> SQL Server Management Studio에 저장된다.
